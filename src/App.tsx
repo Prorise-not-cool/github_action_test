f2eb8fb19174a1e4c2c1535fda1dcfc2fe487643
Prorise <3381292732@qq.com>
2025-11-28 14:59:40 +0800

    Remove debug console.log statement from App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 
 export default function App() {
   const [count, setCount] = useState(0)
-  console.log(count)
   return <div>
     <h1>Hello World</h1>
     <button onClick={() => setCount(count + 1)}>Click me</button>
