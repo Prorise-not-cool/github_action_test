0a588d36973f711d7d8b8789a91d873ca432e542
Prorise <3381292732@qq.com>
2025-11-30 21:07:38 +0800

    feat: display app version in footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { VERSION } from "./version"
 
 export default function App() {
+  const handleClick = () => {
+    alert("Button clicked")
+  }
   return <div>
     <p>Version: {VERSION}</p>
+    <button onClick={handleClick}>Click me</button>
   </div>
 }
