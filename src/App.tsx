634ed5c0f6e0c853b5fd80023b8540b5f59a2e82
Prorise <3381292732@qq.com>
2025-12-01 20:51:32 +0800

    refactor: :recycle: remove counter and button, keep version display

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import { VERSION } from "./version"
-import { useState } from "react"
 export default function App() {
-  const [count, setCount] = useState(0)
 
-  const handleClick = () => {
-    setCount(count + 1)
-  }
+
   return <div>
     <p>Version: {VERSION}</p>
-    <button onClick={handleClick}>Click me</button>
-    <p>Count: {count}</p>
   </div>
 }
