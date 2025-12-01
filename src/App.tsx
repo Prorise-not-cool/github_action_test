9257570b1e35f9436c6fd7ede37136815b34bc64
Prorise <3381292732@qq.com>
2025-12-01 08:57:56 +0800

    feat: :sparkles: add state to track button click count

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { VERSION } from "./version"
-
+import { useState } from "react"
 export default function App() {
+  const [count, setCount] = useState(0)
+
   const handleClick = () => {
-    alert("Button clicked")
+    setCount(count + 1)
   }
   return <div>
     <p>Version: {VERSION}</p>
     <button onClick={handleClick}>Click me</button>
+    <p>Count: {count}</p>
   </div>
 }
