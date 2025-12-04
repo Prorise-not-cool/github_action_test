98279b9b9dcbc6997d8502a237a920a4ef55525c
Prorise <3381292732@qq.com>
2025-12-04 14:53:40 +0800

    Remove hardcoded AWS credentials from config file

--- config.credentials.js
+++
@@ -1,8 +0,0 @@
-// config.credentials.js
-// 这是一个模拟的硬编码密钥场景
-export const awsConfig = {
-    region: "us-east-1",
-    // GitHub 的测试用伪造 Token，会被识别为真实风险
-    accessToken: "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"
-}
-
